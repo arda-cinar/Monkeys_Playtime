347da8d625a79dba548e6b904228e4044b26ce14
Arda Çınar <cinararda71@gmail.com>
2025-12-17 04:15:23 +0300

    v0.3: Karakter Hareketi ve Kamera Sistemi
    Hareket sistemi CharacterController yapısı üzerine yeniden inşa edildi.
    
    Kamera kontrol mekaniği entegre edildi:
    
    Yatay eksende (Mouse X): Ana karakter objesi (Player) döndürülerek gövdenin yön değiştirmesi sağlandı.
    
    Dikey eksende (Mouse Y): Sadece kameranın localRotation değeri değiştirildi. Böylece yukarı-aşağı bakışta gövde sabit kalırken bağımsız kamera hareketi elde edildi.

---
+++ Assets/Scripts/Player/Kamera_Hareketi.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Kamera_Hareketi : MonoBehaviour
+{
+    [Header("Atama")]
+    public Transform Player;
+
+    [Header("Ayar")]
+    public float mouseHassasiyeti = 100f;
+
+    float xRotation = 0f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseHassasiyeti * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseHassasiyeti * Time.deltaTime;
+
+        xRotation -= mouseY; //Sol El (Ters)
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f); //sýnýrlama
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f); // Çeviri / Yerel Eksen
+
+        if (Player != null)
+        {
+            Player.Rotate(Vector3.up * mouseX); //Eksenel Dönüþ
+        }
+    }
+}
---
+++ Assets/Scripts/Player/Player_Hareketi.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+
+public class Player_Hareketi : MonoBehaviour
+{
+    private CharacterController controller;
+
+    public float speed = 8f;
+    public float gravity = -9.81f * 3;
+    public float jumpHeight = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;  //küre yarýçapý
+    public LayerMask groundMask;        //atanan Layer'larýn kimliði
+
+    Vector3 velocity;
+
+    bool isGrounded;
+    
+    bool isMoving;
+    private Vector3 lastPosition = new Vector3(0f, 0f, 0f); //Hareket hafýzasý
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask); //küre hacminde tarama
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f; // Düþüþü Sýfýrla
+        }
+
+
+
+        float x = Input.GetAxis("Horizontal"); //A-D
+        float z = Input.GetAxis("Vertical");  //W-S
+
+        Vector3 move = transform.right * x + transform.forward * z; // Yerel Yön
+
+        controller.Move(move * speed * Time.deltaTime); // Hareketi Uygulama
+
+
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity); // Zýplama Hýzý
+        }
+
+
+
+        velocity.y += gravity * Time.deltaTime; // Yerçekimi Ývmesi
+
+        controller.Move(velocity * Time.deltaTime); // Yerçekimini Uygulama
+
+
+
+        if (lastPosition != gameObject.transform.position && isGrounded == true) //ses/animasyon
+        {
+            isMoving = true;
+        }
+        else
+        {
+            isMoving = false;
+        }
+
+        lastPosition = gameObject.transform.position; //Hafýzayý güncelle
+    }
+
+}
